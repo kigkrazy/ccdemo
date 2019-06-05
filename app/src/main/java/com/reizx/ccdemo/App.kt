4d03c28aea7768cc9c21fde17e72c4a82267fb69
kigkrazy <kigkrazy@163.com>
2019-06-05 15:04:15 +0800

    init

---
+++ app/src/main/java/com/reizx/ccdemo/App.kt
@@ -0,0 +1,17 @@
+package com.reizx.ccdemo
+
+import android.app.Application
+import com.billy.cc.core.component.CC
+
+class App : Application() {
+    companion object {
+        var app: App? = null;
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        CC.enableVerboseLog(true)
+        CC.enableDebug(true)
+        CC.enableRemoteCC(true)
+    }
+}
